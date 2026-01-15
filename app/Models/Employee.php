44a84d095d6189b940b36a11c6886fc4a73c6d42
strrykmz <satyaakun20@gmail.com>
2026-01-15 10:10:55 +0800

    mengatur layout dan fitur index task

---
+++ app/Models/Employee.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = 
+    [
+        'fullname','email','phone_number','address', 'hire_date','departement_id','role_id','status','salary'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardController; 
+use App\Http\Controllers\TaskController; 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('/tasks', TaskController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
